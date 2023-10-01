b6d94f06533bc41276183b9c867b33a3c58494d6
Sabdar Shaik <sabdaralibaigshaik@gmail.com>
2023-10-01 10:16:39 +0530

    update test with sqlexecute and jenkinsfile

--- scripts/test.groovy
+++ scripts/test.groovy
@@ -9,8 +9,12 @@ def sayBye() {
 def executeSql(){
     withCredentials([usernamePassword(credentialsId: 'cloud_oracle_db_hr', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         def query = "SELECT * FROM EMPLOYEES"
-        def sqlplusCommand = "sqlplus -S $USERNAME/$PASSWORD@oci_high <<EOF\n${query}\nEOF"
-        def result = sh(script: sqlplusCommand, returnStdout: true)
+        def tns_path = sh 'echo $TNS_ADMIN'.trim()
+        dir(tns_path){
+            def sqlplusCommand = "sqlplus -S $USERNAME/$PASSWORD@oci_high <<EOF\n${query}\nEOF"
+            def result = sh(script: sqlplusCommand, returnStdout: true)
+        }
+
         writeFile file: 'output.txt', text: result
         echo result
     }
